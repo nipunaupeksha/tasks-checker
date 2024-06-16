14d57d1512c3963231cf827147ecf1f53060629d
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-06-16 18:28:23 +0300

    added project

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { type User } from './user.model';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
